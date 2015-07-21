e0683d603104b97c8fcd2af7c7631d9ac6c3131c
Paulo Pires <taralloqueiroiz@gmail.com>
2015-07-20 19:04:05 -0700

    some code refactor

--- server.js
+++ server.js
@@ -2,46 +2,28 @@ var path = require('path');
 var fs = require('fs');
 var express = require('express');
 var jspt = require('jspt');
-
-// Server part
+var utils = require('./lib/utils')
 var app = express();
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-var server = app.listen(process.env.PORT || 3000);
-console.log('Server listening on port 3000');
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('Server listening on port 3000');
+});
 
-// Socket.IO part
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
 
 	socket.on('newCode', function (code, callback) {
     var fileName = path.resolve('./tmp/', socket.id+'.alg');
-    setTimeout(function(){
-      fs.writeFileSync(fileName, code);
-      openFile(fileName, function (data) {
-        try {
-          var result = jspt.execute(data, createContext());
-          return callback(null, result)
-        } catch (e) {
-          return callback(e.message, null);
-        }
-      });
-    }, 700)
+    fs.writeFileSync(fileName, code);
+    utils.openFile(fileName, function (data) {
+      try {
+        var result = jspt.execute(data, utils.createContext());
+        return callback(null, result)
+      } catch (e) {
+        return callback(e.message, null);
+      }
+    });
 	});
 });
-
-function createContext() {
-  return require('./lib/modules/std').module;
-}
-
-
-function openFile(file, fn) {
-  fs.readFile(file, function(error, data) {
-      if (error) {
-          throw new Error(error);
-      }
-
-      fn(data.toString('utf8'));
-  });
-}
